741f4dfdbb75f2b3245c218a481be02462e439c7
Benjamin Walkenhorst <krylon@posteo.de>
2021-07-30 18:06:13 +0200

    Prettify the cleanup script.

--- clean.sh
+++ clean.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+# Time-stamp: <2021-07-30 18:06:08 krylon>
 
 cd $GOPATH/src/github.com/blicero/dwd/
 
-rm -vf bak.dwd dwd dbg.build.log && du -sh . && git fsck --full && git reflog expire --expire=now && git gc --aggressive --prune=now && du -sh .
+rm -vf bak.dwd dwd dbg.build.log && \
+    du -sh . && \
+    git fsck --full && \
+    git reflog expire --expire=now && \
+    git gc --aggressive --prune=now && \
+    du -sh .
 
